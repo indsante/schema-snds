af845fd9e97da4094490a86b0438f4a73c483c5e
Pierre-Alain Jachiet <pajachiet@protonmail.com>
2019-02-22 16:56:21 +0100

    Generate table sidebar for VuePress Documentation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.add_keys import add_dcirs_keys, add_dcir_keys, add_beneficiary_central_table_DCIR_keys, \
     add_beneficiary_central_table_DCIRS_keys
 from src.convert import table_schema_to_markdown, table_schema_to_sql_within_docker, \
-    table_schema_to_relational_diagram_from_host
+    table_schema_to_relational_diagram_from_host, generate_table_sidebar
 from src.reformat_snds_dico import dico_snds_to_table_schema
 from src.utils import is_running_in_docker
 
@@ -11,6 +11,7 @@ add_dcir_keys()
 add_beneficiary_central_table_DCIR_keys()
 add_beneficiary_central_table_DCIRS_keys()
 table_schema_to_markdown()
+generate_table_sidebar()
 
 if is_running_in_docker():
     table_schema_to_sql_within_docker()

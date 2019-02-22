af5b1814335bd8844c1e1cfda38314fd38a6c13a
viktor.jarry <viktor.jarry@sante.gouv.fr>
2019-02-22 12:33:30 +0100

    Use volume for schemacrawler.config.properties

---
+++ docker/schemacrawler/schemacrawler_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo 'Running schema crawler against PostgreSQL'
+
+/opt/schemacrawler/schemacrawler.sh -command=brief \
+-infolevel=standard \
+-server=postgresql -host=postgres -port=5432 -user=postgres -database=postgres \
+-title 'Schema DCIR et DCIRS' \
+-only-matching \
+-outputformat=pdf -outputfile=/share/schema_postgres.pdf
+
+echo 'Done. Relational diagram has been written in data/diagram'

b90845399d5656e9deb8cb7c04c9b2470d1f1d4e
Pierre-Alain Jachiet <pajachiet@protonmail.com>
2019-03-11 10:55:25 +0100

    feat: table_schema_to_snds_tables

--- src/dico_snds.py
+++ src/dico_snds.py
@@ -12,6 +12,10 @@ import pandas as pd
 from src.constants import APP_DICO_SNDS_PATH
 from src.utils import get_all_schema
 
+DICO_VARIABLE = 'variable'
+DICO_PRODUIT = 'produit'
+SCHEMA_PRODUIT = 'produit'
+
 pd.options.display.max_columns = 100
 
 
@@ -32,21 +36,34 @@ def table_schema_to_snds_variables():
             length = descriptor.get('length', '')
             length = ' ({})'.format(length) if length else ''
             variables_list.append({
-                'produit': schema.descriptor['produit'],
+                DICO_PRODUIT: schema.descriptor[SCHEMA_PRODUIT],
                 'table': schema.descriptor['name'],
-                'var': descriptor['name'],
+                DICO_VARIABLE: descriptor['name'],
                 'format': descriptor['type'] + length,
                 'description': descriptor['description'],
                 'nomenclature': descriptor.get('nomenclature', ''),
             })
-    df = pd.DataFrame(variables_list, columns=['produit', 'table', 'var', 'format', 'description', 'nomenclature'])
-    df = df.sort_values(['produit', 'table', 'var'])
+    df = pd.DataFrame(variables_list,
+                      columns=[DICO_PRODUIT, 'table', DICO_VARIABLE, 'format', 'description', 'nomenclature'])
+    df = df.sort_values([DICO_PRODUIT, 'table', DICO_VARIABLE])
     snds_variable_path = os.path.join(APP_DICO_SNDS_PATH, 'snds_variables.csv')
     df.to_csv(snds_variable_path, index=False)
 
 
 def table_schema_to_snds_tables():
-    pass
+    logging.info("Convert schemas to snds_tables.csv")
+    table_list = []
+    for schema in get_all_schema():
+        table_list.append({
+            DICO_PRODUIT: schema.descriptor[SCHEMA_PRODUIT],
+            'table': schema.descriptor['name'],
+            'libelle': schema.descriptor['title'],
+
+        })
+    df = pd.DataFrame(table_list, columns=[DICO_PRODUIT, 'table', 'libelle'])
+    df = df.sort_values([DICO_PRODUIT, 'table', 'libelle'])
+    snds_table_path = os.path.join(APP_DICO_SNDS_PATH, 'snds_tables.csv')
+    df.to_csv(snds_table_path, index=False)
 
 
 def table_schema_to_snds_graph():

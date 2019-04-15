f6ca0660eb772bf791d48a332e8f7cff6d4e2904
Pierre-Alain Jachiet <pajachiet@protonmail.com>
2019-04-15 17:57:42 +0200

    Ajout TRIPaa_nnR3A et refactor code add_keys
    - correction de la source SNDS_tables_lib.csc

--- src/build_tableschema/add_keys.py
+++ src/build_tableschema/add_keys.py
@@ -8,10 +8,10 @@ from src.constants import DCIRS_SCHMEMA_DIR, DCIR_SCHMEMA_DIR, DCIR_DCIRS_SCHEMA
     DECES_SCHEMA_DIR, CARTO_PATHO_SCHEMA_DIR, PMSI_MCO_SCHEMA_DIR, PMSI_HAD_SCHEMA_DIR, PMSI_RIMP_SCHEMA_DIR, \
     PMSI_SSR_SCHEMA_DIR
 
-DCIRS_CENTRAL_TABLE = 'NS_PRS_F'
-DCIRS_JOIN_KEY = ['CLE_DCI_JNT']
+NS_PRS_F = 'NS_PRS_F'
+CLE_DCI_JNT = ['CLE_DCI_JNT']
 
-DCIR_CENTRAL_TABLE = 'ER_PRS_F'
+ER_PRS_F = 'ER_PRS_F'
 DCIR_JOIN_KEY = [
     'DCT_ORD_NUM',
     'FLX_DIS_DTD',
@@ -24,29 +24,21 @@ DCIR_JOIN_KEY = [
     'REM_TYP_AFF',
 ]
 
-BENEFICIARY_CENTRAL_TABLE_DCIR = 'IR_BEN_R'
-BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY = [
+IR_BEN_R = 'IR_BEN_R'
+DCIR_BENEFICIARY_JOIN_KEY = [
     'BEN_NIR_PSA',
     'BEN_RNG_GEM',
 ]
-BENEFICIARY_DCIR_EXCLUDED_TABLES = 'DA_PRA_R.json'
-BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY_LOWERCASE = [x.lower() for x in BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY]
+DCIR_BENEFICIARY_JOIN_KEY_lowercase = [x.lower() for x in DCIR_BENEFICIARY_JOIN_KEY]
 
-BENEFICIARY_CENTRAL_TABLE_DCIRS = 'IR_IBA_R'
-BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY = ['BEN_IDT_ANO']
-BENEFICIARY_DCIRS_EXCLUDED_TABLES = [
-    'DA_PRA_R.json',
-    'IR_IMB_R.json',
-]
+DA_PRA_R_json = 'DA_PRA_R.json'
+
+IR_IBA_R = 'IR_IBA_R'
+BEN_IDT_ANO = ['BEN_IDT_ANO']
 
 DECES_JOIN_KEY = ['BEN_IDT_ANO']
 
-PS_JOIN_KEY = 'PFS_PFS_NUM'
-PS_CHAMP_DCIR_DCIRS = [
-    'PFS_EXE_NUM',
-    'PFS_PRE_NUM',
-    'PRS_MTT_NUM',
-]
+PFS_PFS_NUM = 'PFS_PFS_NUM'
 
 CARTO_PATHO_CENTRAL_TABLE = 'CT_IDE_AAAA_GN'
 CARTO_PATHO_JOIN_KEY = "id_carto"
@@ -56,11 +48,11 @@ PMSI_MCO_JOIN_KEY = [
     'ETA_NUM',
     'RSA_NUM',
 ]
+PMSI_MCO_EXT_CENTRAL_TABLE = 'T_MCOaa_nnCSTC'
 PMSI_MCO_EXT_JOIN_KEY = [
     'ETA_NUM',
     'SEQ_NUM'
 ]
-PMSI_MCO_EXT_CENTRAL_TABLE = 'T_MCOaa_nnCSTC'
 
 PMSI_HAD_CENTRAL_TABLE = 'T_HADaa_nnC'
 PMSI_HAD_JOIN_KEY = [
@@ -93,7 +85,6 @@ PMSI_ETAB_TABLES = [
     'T_HADaa_nnE.json'
 ]
 
-
 PMSI_TABLES_LINKED_TO_SNIIRAM = [
     [PMSI_MCO_CENTRAL_TABLE + ".json", PMSI_MCO_SCHEMA_DIR],
     [PMSI_MCO_EXT_CENTRAL_TABLE + ".json", PMSI_MCO_SCHEMA_DIR],
@@ -119,7 +110,7 @@ def add_foreign_key(schema: Schema, fields: Union[str, List[str]], referenced_ta
         'reference': {
             'resource': referenced_table,
             'fields': referenced_fields
-        }
+        },
     }
     schema.descriptor['foreignKeys'].append(foreign_key_descriptor)
     schema.commit(strict=True)
@@ -137,12 +128,12 @@ def add_dcirs_keys() -> None:
     for tableschema_filename in os.listdir(DCIRS_SCHMEMA_DIR):
         path = os.path.join(DCIRS_SCHMEMA_DIR, tableschema_filename)
         schema = Schema(path)
-        if tableschema_filename == DCIRS_CENTRAL_TABLE + '.json':
-            add_primary_key(schema, DCIRS_JOIN_KEY)
+        if tableschema_filename == NS_PRS_F + '.json':
+            add_primary_key(schema, CLE_DCI_JNT)
         else:
-            add_foreign_key(schema, DCIRS_JOIN_KEY, DCIRS_CENTRAL_TABLE, DCIRS_JOIN_KEY)
-            add_foreign_key(schema, BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY, BENEFICIARY_CENTRAL_TABLE_DCIRS,
-                            BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY)
+            add_foreign_key(schema, CLE_DCI_JNT, NS_PRS_F, CLE_DCI_JNT)
+            add_foreign_key(schema, BEN_IDT_ANO, IR_IBA_R,
+                            BEN_IDT_ANO)
         schema.save(path, ensure_ascii=False)
     path_dcirs = os.path.join(DCIRS_SCHMEMA_DIR, 'NS_PRS_F.json')
     schema_dcirs = Schema(path_dcirs)
@@ -162,12 +153,12 @@ def add_dcir_keys() -> None:
     for tableschema_filename in os.listdir(DCIR_SCHMEMA_DIR):
         path = os.path.join(DCIR_SCHMEMA_DIR, tableschema_filename)
         schema = Schema(path)
-        if tableschema_filename == DCIR_CENTRAL_TABLE + '.json':
+        if tableschema_filename == ER_PRS_F + '.json':
             add_primary_key(schema, DCIR_JOIN_KEY)
-            add_foreign_key(schema, BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY, CARTO_PATHO_CENTRAL_TABLE,
-                            BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY_LOWERCASE)
+            add_foreign_key(schema, DCIR_BENEFICIARY_JOIN_KEY, CARTO_PATHO_CENTRAL_TABLE,
+                            DCIR_BENEFICIARY_JOIN_KEY_lowercase)
         else:
-            add_foreign_key(schema, DCIR_JOIN_KEY, DCIR_CENTRAL_TABLE, DCIR_JOIN_KEY)
+            add_foreign_key(schema, DCIR_JOIN_KEY, ER_PRS_F, DCIR_JOIN_KEY)
         schema.save(path, ensure_ascii=False)
     path_dcir = os.path.join(DCIR_SCHMEMA_DIR, 'ER_PRS_F.json')
     schema_dcir = Schema(path_dcir)
@@ -196,16 +187,16 @@ def add_beneficiary_central_table_DCIR_keys() -> None:
     """
     logging.info("Ajout des liens entre la table réferentiel beneficiaire du DCIR IR_BEN_R et "
                  "ses tables associés beneficiaires dans le table schema")
-    path_beneficiary_dcir = os.path.join(BENEFICIARY_SCHEMA_DIR, BENEFICIARY_CENTRAL_TABLE_DCIR + '.json')
+    path_beneficiary_dcir = os.path.join(BENEFICIARY_SCHEMA_DIR, IR_BEN_R + '.json')
     schema_beneficiary_dcir = Schema(path_beneficiary_dcir)
     update_descriptor_field(schema_beneficiary_dcir, 'BEN_IDT_ANO', {"constraints": {"unique": True}})
-    add_primary_key(schema_beneficiary_dcir, BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY)
+    add_primary_key(schema_beneficiary_dcir, DCIR_BENEFICIARY_JOIN_KEY)
     schema_beneficiary_dcir.save(path_beneficiary_dcir, ensure_ascii=False)
-    add_associated_beneficiary_tables_foreign_keys(BENEFICIARY_CENTRAL_TABLE_DCIR,
-                                                   BENEFICIARY_DCIR_EXCLUDED_TABLES,
-                                                   BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY
+    add_associated_beneficiary_tables_foreign_keys(IR_BEN_R,
+                                                   DA_PRA_R_json,
+                                                   DCIR_BENEFICIARY_JOIN_KEY
                                                    )
-    add_deces_foreign_keys_with_beneficiary(BENEFICIARY_CENTRAL_TABLE_DCIR)
+    add_deces_foreign_keys_with_beneficiary(IR_BEN_R)
 
 
 def add_beneficiary_central_table_DCIRS_keys() -> None:
@@ -219,15 +210,15 @@ def add_beneficiary_central_table_DCIRS_keys() -> None:
     """
     logging.info("Ajout des liens entre la table réferentiel beneficiaire du DCIRS IR_IBA_R et ses tables associés "
                  "beneficiaires dans le table schema")
-    path_beneficiary_dcirs = os.path.join(BENEFICIARY_SCHEMA_DIR, BENEFICIARY_CENTRAL_TABLE_DCIRS + '.json')
+    path_beneficiary_dcirs = os.path.join(BENEFICIARY_SCHEMA_DIR, IR_IBA_R + '.json')
     schema_beneficiary_dcirs = Schema(path_beneficiary_dcirs)
-    add_primary_key(schema_beneficiary_dcirs, BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY)
+    add_primary_key(schema_beneficiary_dcirs, BEN_IDT_ANO)
     schema_beneficiary_dcirs.save(path_beneficiary_dcirs, ensure_ascii=False)
-    add_associated_beneficiary_tables_foreign_keys(BENEFICIARY_CENTRAL_TABLE_DCIRS,
-                                                   BENEFICIARY_DCIRS_EXCLUDED_TABLES,
-                                                   BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY
+    add_associated_beneficiary_tables_foreign_keys(IR_IBA_R,
+                                                   ['DA_PRA_R.json', 'IR_IMB_R.json'],
+                                                   BEN_IDT_ANO
                                                    )
-    add_deces_foreign_keys_with_beneficiary(BENEFICIARY_CENTRAL_TABLE_DCIRS)
+    add_deces_foreign_keys_with_beneficiary(IR_IBA_R)
 
 
 def add_associated_beneficiary_tables_foreign_keys(beneficiary_central_table: str,
@@ -268,15 +259,15 @@ def add_DCIR_beneficiary_link() -> None:
 
     """
     logging.info("Ajout des liens entre la table bénéficiaire du DCIR et les tables de prestations")
-    path_dcir = os.path.join(DCIR_SCHMEMA_DIR, DCIR_CENTRAL_TABLE + ".json")
+    path_dcir = os.path.join(DCIR_SCHMEMA_DIR, ER_PRS_F + ".json")
     schema_dcir = Schema(path_dcir)
-    add_foreign_key(schema_dcir, BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY, BENEFICIARY_CENTRAL_TABLE_DCIR,
-                    BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY)
+    add_foreign_key(schema_dcir, DCIR_BENEFICIARY_JOIN_KEY, IR_BEN_R,
+                    DCIR_BENEFICIARY_JOIN_KEY)
     schema_dcir.save(path_dcir, ensure_ascii=False)
-    path_dcirs = os.path.join(DCIRS_SCHMEMA_DIR, DCIRS_CENTRAL_TABLE + ".json")
+    path_dcirs = os.path.join(DCIRS_SCHMEMA_DIR, NS_PRS_F + ".json")
     schema_dcirs = Schema(path_dcirs)
-    add_foreign_key(schema_dcirs, BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY, BENEFICIARY_CENTRAL_TABLE_DCIR,
-                    BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY)
+    add_foreign_key(schema_dcirs, BEN_IDT_ANO, IR_BEN_R,
+                    BEN_IDT_ANO)
     schema_dcirs.save(path_dcirs, ensure_ascii=False)
 
 
@@ -286,10 +277,10 @@ def add_DCIRS_beneficiary_link() -> None:
 
     """
     logging.info("Ajout des liens entre la table bénéficiaire du DCIRS et les tables de prestations")
-    path = os.path.join(DCIRS_SCHMEMA_DIR, DCIRS_CENTRAL_TABLE + ".json")
+    path = os.path.join(DCIRS_SCHMEMA_DIR, NS_PRS_F + ".json")
     schema = Schema(path)
-    add_foreign_key(schema, BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY, BENEFICIARY_CENTRAL_TABLE_DCIRS,
-                    BENEFICIARY_CENTRAL_TABLE_DCIRS_JOIN_KEY)
+    add_foreign_key(schema, BEN_IDT_ANO, IR_IBA_R,
+                    BEN_IDT_ANO)
     schema.save(path, ensure_ascii=False)
 
 
@@ -300,7 +291,7 @@ def add_DA_PRA_R_keys() -> None:
     logging.info("Ajout des clefs primaires à la table des professionels de santé DA_PRA_R")
     path = os.path.join(DCIR_DCIRS_SCHEMA_DIR, "DA_PRA_R.json")
     schema = Schema(path)
-    add_primary_key(schema, PS_JOIN_KEY)
+    add_primary_key(schema, PFS_PFS_NUM)
     schema.save(path, ensure_ascii=False)
 
 
@@ -311,8 +302,8 @@ def add_da_pra_r_foreign_keys(schema: Schema, path: Union[bytes, str]) -> None:
     Fonction appelée dans la génération des clefs du DCIR et du DCIRS. Pour les trois champs possibles (PFS_EXE_NUM,
     PFS_PRE_NUM, PRS_MTT_NUM) on crée une clef étrangère pointant la clef principale du DA_PRA_R : PFS_PFS_NUM
     """
-    for ps_join_foreign_key in PS_CHAMP_DCIR_DCIRS:
-        add_foreign_key(schema, ps_join_foreign_key, 'DA_PRA_R', PS_JOIN_KEY)
+    for ps_join_foreign_key in ['PFS_EXE_NUM', 'PFS_PRE_NUM', 'PRS_MTT_NUM']:
+        add_foreign_key(schema, ps_join_foreign_key, 'DA_PRA_R', PFS_PFS_NUM)
         schema.save(path, ensure_ascii=False)
 
 
@@ -328,8 +319,8 @@ def add_cartographie_pathologies_dcir_dircs_foreign_keys() -> None:
         path = os.path.join(DCIR_DCIRS_SCHEMA_DIR, tableschema_filename)
         schema = Schema(path)
         if tableschema_filename not in 'DA_PRA_R.json':
-            add_foreign_key(schema, BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY, CARTO_PATHO_CENTRAL_TABLE,
-                            BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY_LOWERCASE)
+            add_foreign_key(schema, DCIR_BENEFICIARY_JOIN_KEY, CARTO_PATHO_CENTRAL_TABLE,
+                            DCIR_BENEFICIARY_JOIN_KEY_lowercase)
         schema.save(path, ensure_ascii=False)
 
 
@@ -350,9 +341,9 @@ def add_cartographie_pathologies_foreign_keys() -> None:
         schema = Schema(path)
         if tableschema_filename == CARTO_PATHO_CENTRAL_TABLE + '.json':
             schema.update_field('id_carto', {"constraints": {"unique": True}})
-            add_primary_key(schema, BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY_LOWERCASE)
-            add_foreign_key(schema, BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY_LOWERCASE, BENEFICIARY_CENTRAL_TABLE_DCIR,
-                            BENEFICIARY_CENTRAL_TABLE_DCIR_JOIN_KEY)
+            add_primary_key(schema, DCIR_BENEFICIARY_JOIN_KEY_lowercase)
+            add_foreign_key(schema, DCIR_BENEFICIARY_JOIN_KEY_lowercase, IR_BEN_R,
+                            DCIR_BENEFICIARY_JOIN_KEY)
         else:
             add_foreign_key(schema, CARTO_PATHO_JOIN_KEY, CARTO_PATHO_CENTRAL_TABLE, CARTO_PATHO_JOIN_KEY)
         schema.save(path, ensure_ascii=False)
@@ -389,7 +380,7 @@ def add_pmsi_mco_etablissement_keys() -> None:
     schema_etab = Schema(path_etab)
     add_primary_key(schema_etab, 'ETA_NUM')
     schema_etab.save(path_etab, ensure_ascii=False)
-    for tableschema_filename in ['T_MCOaa_nnC.json', 'T_MCOaa_nnCSTC.json']:
+    for tableschema_filename in [PMSI_MCO_CENTRAL_TABLE + '.json', PMSI_MCO_EXT_CENTRAL_TABLE + '.json']:
         path = os.path.join(PMSI_MCO_SCHEMA_DIR, tableschema_filename)
         schema = Schema(path)
         add_foreign_key(schema, 'ETA_NUM', 'T_MCOaa_nnE', 'ETA_NUM')
@@ -409,7 +400,6 @@ def add_pmsi_mco_actes_ext_keys() -> None:
         else:
             add_foreign_key(schema, PMSI_MCO_EXT_JOIN_KEY, PMSI_MCO_EXT_CENTRAL_TABLE, PMSI_MCO_EXT_JOIN_KEY)
         schema.save(path, ensure_ascii=False)
-
 
 
 def add_pmsi_had_keys() -> None:
@@ -444,7 +434,7 @@ def add_pmsi_had_etablissement_keys() -> None:
         schema_etab = Schema(path_etab)
         add_primary_key(schema_etab, tableschema_keys_etab_had[1])
         schema_etab.save(path_etab, ensure_ascii=False)
-        path = os.path.join(PMSI_HAD_SCHEMA_DIR, 'T_HADaa_nnC.json')
+        path = os.path.join(PMSI_HAD_SCHEMA_DIR, PMSI_HAD_CENTRAL_TABLE + '.json')
         schema = Schema(path)
         add_foreign_key(schema, 'ETA_NUM_EPMSI', tableschema_keys_etab_had[0], tableschema_keys_etab_had[1])
         schema.save(path, ensure_ascii=False)
@@ -462,7 +452,8 @@ def add_pmsi_rimp_keys() -> None:
         schema = Schema(path)
         if tableschema_filename == PMSI_RIMP_CENTRAL_TABLE + '.json':
             add_primary_key(schema, PMSI_RIMP_JOIN_KEY)
-        elif tableschema_filename not in ['T_RIPaa_nnE.json', 'T_RIPaa_nnR3AD.json', PMSI_RIMP_CENTRAL_TABLE + '.json']:
+        elif tableschema_filename not in ['T_RIPaa_nnE.json', 'T_RIPaa_nnR3A.json', 'T_RIPaa_nnR3AD.json',
+                                          PMSI_RIMP_CENTRAL_TABLE + '.json']:
             add_foreign_key(schema, PMSI_RIMP_JOIN_KEY, PMSI_RIMP_CENTRAL_TABLE, PMSI_RIMP_JOIN_KEY)
         schema.save(path, ensure_ascii=False)
     add_pmsi_rimp_etablissement_keys()
@@ -477,7 +468,7 @@ def add_pmsi_rimp_etablissement_keys() -> None:
     schema_etab = Schema(path_etab)
     add_primary_key(schema_etab, 'ETA_NUM')
     schema_etab.save(path_etab, ensure_ascii=False)
-    path = os.path.join(PMSI_RIMP_SCHEMA_DIR, 'T_RIPaa_nnC.json')
+    path = os.path.join(PMSI_RIMP_SCHEMA_DIR, PMSI_RIMP_CENTRAL_TABLE + '.json')
     schema = Schema(path)
     add_foreign_key(schema, 'ETA_NUM_EPMSI', 'T_RIPaa_nnE', 'ETA_NUM')
     schema.save(path, ensure_ascii=False)
@@ -513,7 +504,7 @@ def add_pmsi_ssr_keys() -> None:
         schema = Schema(path)
         if tableschema_filename == PMSI_SSR_CENTRAL_TABLE + '.json':
             add_primary_key(schema, PMSI_SSR_JOIN_KEY)
-        elif tableschema_filename not in ['T_SSRaa_nnCSTC.json', PMSI_SSR_CENTRAL_TABLE + '.json',
+        elif tableschema_filename not in ['T_SSRaa_nnCSTC.json', 'T_SSRaa_nnC.json',
                                           'T_SSRaa_nnE.json', 'T_SSRaa_nnFASTC.json', 'T_SSRaa_nnFBSTC.json',
                                           'T_SSRaa_nnFCSTC.json', 'T_SSRaa_nnFLSTC.json', 'T_SSRaa_nnFMSTC.json']:
             add_foreign_key(schema, PMSI_SSR_JOIN_KEY, PMSI_SSR_CENTRAL_TABLE, PMSI_SSR_JOIN_KEY)
@@ -531,7 +522,7 @@ def add_pmsi_ssr_etablissement_keys() -> None:
     schema_etab = Schema(path_etab)
     add_primary_key(schema_etab, 'ETA_NUM')
     schema_etab.save(path_etab, ensure_ascii=False)
-    for tableschema_filename in ['T_SSRaa_nnC.json', 'T_SSRaa_nnCSTC.json']:
+    for tableschema_filename in [PMSI_SSR_CENTRAL_TABLE + '.json', PMSI_SSR_EXT_CENTRAL_TABLE + '.json']:
         path = os.path.join(PMSI_SSR_SCHEMA_DIR, tableschema_filename)
         schema = Schema(path)
         add_foreign_key(schema, 'ETA_NUM', 'T_SSRaa_nnE', 'ETA_NUM')
@@ -546,8 +537,8 @@ def add_unicity_constraint_DCIR() -> None:
     Cette contrainte d'unicité est obligatoire pour le lien avec postgres ne se vérifie pas dans les faits.
     Le PMSI n'a pas le rang gemelaire et ne permet donc pas de différencier 2 personnes jumelles de même sexe.
     """
-    for tableschema_filename_dir_list in [[BENEFICIARY_CENTRAL_TABLE_DCIR, BENEFICIARY_SCHEMA_DIR],
-                                          [DCIR_CENTRAL_TABLE, DCIR_SCHMEMA_DIR]]:
+    for tableschema_filename_dir_list in [[IR_BEN_R, BENEFICIARY_SCHEMA_DIR],
+                                          [ER_PRS_F, DCIR_SCHMEMA_DIR]]:
         path = os.path.join(tableschema_filename_dir_list[1], tableschema_filename_dir_list[0] + ".json")
         schema = Schema(path)
         update_descriptor_field(schema, 'BEN_NIR_PSA', {"constraints": {"unique": True}})
@@ -566,8 +557,8 @@ def add_pmsi_dcir_link() -> None:
     for tableschema_filename_dir_list in PMSI_TABLES_LINKED_TO_SNIIRAM:
         path = os.path.join(tableschema_filename_dir_list[1], tableschema_filename_dir_list[0])
         schema = Schema(path)
-        add_foreign_key(schema, 'NIR_ANO_17', BENEFICIARY_CENTRAL_TABLE_DCIR, 'BEN_NIR_PSA')
-        add_foreign_key(schema, 'NIR_ANO_17', DCIR_CENTRAL_TABLE, 'BEN_NIR_PSA')
+        add_foreign_key(schema, 'NIR_ANO_17', IR_BEN_R, 'BEN_NIR_PSA')
+        add_foreign_key(schema, 'NIR_ANO_17', ER_PRS_F, 'BEN_NIR_PSA')
         schema.save(path, ensure_ascii=False)
 
 

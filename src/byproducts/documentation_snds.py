287115f4faeb98483b525e66d8c6240c3f66e609
maevakos <maeva.kos@health-data-hub.fr>
2020-02-12 14:47:48 +0100

    feat: added schema json to md generation

--- src/byproducts/documentation_snds.py
+++ src/byproducts/documentation_snds.py
@@ -1,20 +1,32 @@
-import logging
-import os
-
-from src.constants import ROOT_DIR, SCHEMAS_DIR, SCHEMAS_DOCUMENTATION_SNDS_DIR
-from os.path import join as pjoin
 import shutil
+from os.path import join as pjoin
+from os.path import exists
+from pathlib import Path
+from src.constants import SCHEMAS_DIR, SCHEMAS_MD_DIR, TEMPLATES_DIR, ROOT_DIR
+from src.utils import get_all_schema_path
+import subprocess
 
 
-def generate_documentation_snds(work_dir) -> None:
-    rooted_schemas_dir = pjoin(work_dir, SCHEMAS_DIR)
-    rooted_schemas_documentation_snds_dir = pjoin(work_dir, SCHEMAS_DOCUMENTATION_SNDS_DIR)
-    logging.info("Copy schemas for documentation in directory '{}'"
-                 .format(rooted_schemas_documentation_snds_dir))
-    if os.path.exists(rooted_schemas_documentation_snds_dir):
-        shutil.rmtree(rooted_schemas_documentation_snds_dir)
-    shutil.copytree(rooted_schemas_dir, rooted_schemas_documentation_snds_dir)
+def convert_schema_to_md(file_path, template, fields_format):
+    file_path_dst = file_path.replace(SCHEMAS_DIR, SCHEMAS_MD_DIR)
+    schemas_md_path = Path(file_path_dst).parent.as_posix().replace(" ", "\\ ")
+    subprocess.run("mkdir -p " + schemas_md_path, shell=True)
+
+    cmd = "table-schema-to-markdown " + file_path.replace(" ", "\\ ") + \
+          " --template=" + template + " --fields-format="+fields_format+" >> " + \
+          file_path_dst.replace(".json", ".md").replace(" ", "\\ ")
+
+    subprocess.run(cmd, shell=True)
+
+
+def generate_schema_md(work_dir):
+    # delete the current generated md schemas if exist
+    if exists(pjoin(work_dir, SCHEMAS_MD_DIR)):
+        shutil.rmtree(pjoin(work_dir, SCHEMAS_MD_DIR))
+
+    for schema in get_all_schema_path(work_dir):
+        convert_schema_to_md(schema, TEMPLATES_DIR+"/template_schema.hbs", "table")
 
 
 if __name__ == '__main__':
-    generate_documentation_snds(ROOT_DIR)
+    generate_schema_md(ROOT_DIR)
--- src/byproducts/main.py
+++ src/byproducts/main.py
@@ -1,17 +1,11 @@
 import logging
 import shutil
-import os
-from os.path import exists
 from os.path import join as pjoin
-import subprocess
 from src.byproducts.dico_snds import generate_dico_snds
-from src.byproducts.documentation_snds import generate_documentation_snds
+from src.byproducts.documentation_snds import generate_schema_md
 from src.byproducts.relational_diagram import generate_relational_diagram
 from src.byproducts.tsfaker_schemas import generate_synthetic_snds
 from src.constants import BYPRODUCTS_DIR
-from pathlib import Path
-from src.constants import SCHEMAS_DIR, SCHEMAS_MD_DIR, TEMPLATES_DIR
-from src.utils import get_all_schema_path
 
 
 def generate_byproducts(generate_erd, work_dir):
@@ -21,7 +15,7 @@ def generate_byproducts(generate_erd, work_dir):
     shutil.rmtree(rooted_byproducts_dir, ignore_errors=True)
 
     logging.info("Génération des produits dérivés dans le dossier '{}'.".format(rooted_byproducts_dir))
-    generate_documentation_snds(work_dir)
+    generate_schema_md(work_dir)
     generate_dico_snds(work_dir)
     generate_synthetic_snds(work_dir)
 
@@ -29,23 +23,4 @@ def generate_byproducts(generate_erd, work_dir):
         generate_relational_diagram(work_dir)
 
 
-def convert_schema_to_md(file_path, template, fields_format):
-    file_path_dst = file_path.replace(SCHEMAS_DIR, SCHEMAS_MD_DIR)
-    schemas_md_path = Path(file_path_dst).parent.as_posix().replace(" ", "\\ ")
-    subprocess.run("mkdir -p " + schemas_md_path, shell=True)
-
-    cmd = "table-schema-to-markdown " + file_path.replace(" ", "\\ ") + \
-          " --template=" + template + " --fields-format="+fields_format+" >> " + \
-          file_path_dst.replace(".json", ".md").replace(" ", "\\ ")
-
-    subprocess.run(cmd, shell=True)
-
-
-def generate_schema_md():
-    # delete the current generated md schemas if exist
-    if exists(SCHEMAS_MD_DIR):
-        shutil.rmtree(SCHEMAS_MD_DIR)
-
-    for schema in get_all_schema_path('.'):
-        convert_schema_to_md(schema, TEMPLATES_DIR+"/template_schema.hbs", "table")
 
